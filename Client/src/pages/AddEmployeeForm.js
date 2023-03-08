7cacbc2d44f0b0bffeaf9411930a549b5a03ae69
Tehseen Raza <tehseen.raza@icgames.com>
2023-03-08 11:53:55 +0500

    Employee adding and posting employee form data to api complete

--- Client/src/pages/AddEmployeeForm.js
+++ Client/src/pages/AddEmployeeForm.js
@@ -4,40 +4,86 @@ import React, { useState } from 'react'
 
 function AddEmployeeForm() {
 
+  const [formValues, setFormValues] = useState({
+    fName: '',
+    lName: '',
+    email: '',
+    contact: '',
+    age: '',
+    dob: ''
+  });
 
+  const [message, setMessage] = useState();
 
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const allInputValues = {
+      fName: formValues.fName,
+      lName: formValues.lName,
+      email: formValues.email,
+      contact: formValues.contact,
+      age: formValues.age,
+      dob: formValues.dob
+    }
+
+    const url = 'http://localhost:5000/api/employee/add';
+    let res = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(allInputValues)
+    });
+    // let respjson = await res.json();
+    if (res.status === 200) {
+      setMessage('Data Added Successfully.')
+    }
+
+  }
+
+  const redirectMe = () => {
+    setTimeout(() => {
+      location.replace("http://localhost:3000/")
+    }, 3000)
+  }
 
   return (
-    <form className="row g-3">
-      <div className="col-md-6">
-        <label for='fName' className="form-label">First Name</label>
-        <input type="text" className="form-control" id='fName' />
-      </div>
-      <div className="col-md-6">
-        <label for='lName' className="form-label">Last Name</label>
-        <input type="text" className="form-control" id='lName' />
-      </div>
-      <div className="col-md-6">
-        <label for='email' className="form-label">Email Address</label>
-        <input type="email" className="form-control" id='email' />
-      </div>
-      <div className="col-md-6">
-        <label for='cNumber' className="form-label">Contact Number</label>
-        <input type="number" className="form-control" id='cNumber' />
-      </div>
-      <div className="col-md-6">
-        <label for='age' className="form-label">Enter Your Age</label>
-        <input type="number" className="form-control" id='age' />
-      </div>
-      <div className="col-md-6">
-        <label for='dob' className="form-label">Date Of Birth</label>
-        <input type='date' className='form-control' id='dob' />
-      </div>
-      <div className="col-12 text-end">
-        <button type="submit" className="btn btn-primary">Add Employee</button>
-      </div>
-    </form>
+    <>
+      <p>{message}</p>
+      <form className="row g-3" onSubmit={handleSubmit} method='POST'>
+        <div className="col-md-6">
+          <label className="form-label">First Name</label>
+          <input type="text" name='fName' className="form-control" value={formValues.fName} onChange={handleInput} />
+        </div>
+        <div className="col-md-6">
+          <label className="form-label">Last Name</label>
+          <input type="text" name='lName' className="form-control" value={formValues.lName} onChange={handleInput} />
+        </div>
+        <div className="col-md-6">
+          <label className="form-label">Email Address</label>
+          <input type="email" name='email' className="form-control" value={formValues.email} onChange={handleInput} />
+        </div>
+        <div className="col-md-6">
+          <label className="form-label">Contact Number</label>
+          <input type="number" name='contact' className="form-control" value={formValues.contact} onChange={handleInput} />
+        </div>
+        <div className="col-md-6">
+          <label className="form-label">Enter Your Age</label>
+          <input min='18' max='40' type="number" name='age' className="form-control" value={formValues.age} onChange={handleInput} />
+        </div>
+        <div className="col-md-6">
+          <label className="form-label">Date Of Birth</label>
+          <input type='date' name='dob' className='form-control' value={formValues.age} onChange={handleInput} />
+        </div>
+        <div className="col-12 text-end">
+          <button type="submit" onClick={redirectMe} className="btn btn-primary">Add Employee</button>
+        </div>
+      </form>
+    </>
 
   )
 }
